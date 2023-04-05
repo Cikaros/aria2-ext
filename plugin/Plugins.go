5993710fef86915fd962c63adc2868b1aa19fb07
Cikaros <cikaros@qq.com>
2023-04-05 17:24:23 +0800

    fix plugin path

--- plugin/Plugins.go
+++ plugin/Plugins.go
@@ -26,7 +26,7 @@ func loadPlugins(pluginPath string) {
 	}
 	for _, file := range dirs {
 		if !file.IsDir() {
-			p, err := plugin.Open(pluginPath + file.Name())
+			p, err := plugin.Open(pluginPath + "/" + file.Name())
 			if err != nil {
 				log.Printf("Unable to open %s, load has been skipped... Procedure: %v\n", file.Name(), err)
 				return
