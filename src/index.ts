276bde65fc2ad75761570b1752ed43e75ae98fb6
Cikaros <Cikaros@qq.com>
2024-04-11 20:13:16 +0800

    add reboot指令功能

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,14 @@ import bot from "./lib/bot";
 import db from "./lib/db";
 import rss from "./lib/rss";
 import downloader from "./lib/downloader";
+import {MsgType} from "matrix-js-sdk";
+
+bot.addCommend({
+    obj: this,
+    regex: /^reboot$/,
+    type: MsgType.Text,
+    handler: ()=> process.kill(process.pid, 'SIGTERM')
+});
 
 process.on("SIGTERM", async () => {
     rss.stop();
